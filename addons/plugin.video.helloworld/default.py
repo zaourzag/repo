72ab6781ddcbe498c7099502409ad88938a43642
root <root@vogler.name>
2015-09-08 21:51:17 +0200

    Test Plugin

---
+++ addons/plugin.video.helloworld/default.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import urlparse
+import xbmcgui
+import xbmcplugin
+import xbmcaddon
+import xbmc
+import xbmcvfs
+import urllib, urllib2, socket, cookielib, re, os, shutil,json
+
+
+
+# Setting Variablen Des Plugins
+global debuging
+base_url = sys.argv[0]
+addon_handle = int(sys.argv[1])
+args = urlparse.parse_qs(sys.argv[2][1:])
+addon = xbmcaddon.Addon()
+# Lade Sprach Variablen
+translation = addon.getLocalizedString
+debuging=""
+# Es geht um Videos
+xbmcplugin.setContent(addon_handle, 'movies')
+baseurl="http://www.br.de"
+icon = xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('path')+'/icon.png').decode('utf-8')
+useThumbAsFanart=addon.getSetting("useThumbAsFanart") == "true"
+defaultBackground = ""
+defaultThumb = ""
+global bitrate
+bitrate=addon.getSetting("bitrate")
+global kurzvideos
+kurzvideos=addon.getSetting("kurzvideos")
+
+profile    = xbmc.translatePath( addon.getAddonInfo('profile') ).decode("utf-8")
+temp       = xbmc.translatePath( os.path.join( profile, 'temp', '') ).decode("utf-8")
+
+if xbmcvfs.exists(temp):
+  shutil.rmtree(temp)
+xbmcvfs.mkdirs(temp)
+
+def debug(content):
+    log(content, xbmc.LOGDEBUG)
+    
+def notice(content):
+    log(content, xbmc.LOGNOTICE)
+
+def log(msg, level=xbmc.LOGNOTICE):
+    addon = xbmcaddon.Addon()
+    addonID = addon.getAddonInfo('id')
+    xbmc.log('%s: %s' % (addonID, msg), level) 
+    
+  
+def addLink(name, url, mode, iconimage, duration="", desc="", genre=''):
+	u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)
+	ok = True
+	liz = xbmcgui.ListItem(name, iconImage=defaultThumb, thumbnailImage=iconimage)
+	liz.setInfo(type="Video", infoLabels={"Title": name, "Plot": desc, "Genre": genre})
+	liz.setProperty('IsPlayable', 'true')
+	liz.addStreamInfo('video', { 'duration' : duration })
+	liz.setProperty("fanart_image", iconimage)
+	#liz.setProperty("fanart_image", defaultBackground)
+	xbmcplugin.setContent(int(sys.argv[1]), 'tvshows')
+	ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz)
+	return ok
+
+def playlive(url):
+    url = "http://org-www.arte.tv/papi/tvguide/videos/livestream/player/D/"    
+    content = getUrl(url)
+    match = re.compile('"RMTP_HQ":\\{"quality":"SD - 400p","width":.+?,"height":.+?,"mediaType":"rtmp","mimeType":"application/x-fcs","bitrate":.+?,"streamer":"(.+?)","url":"(.+?)"', re.DOTALL).findall(content)
+    listitem = xbmcgui.ListItem(path=match[0][0] + match[0][1] + " swfUrl=http://www.arte.tv/flash/mediaplayer/mediaplayer.swf live=1 swfVfy=1")        
+    xbmcplugin.setResolvedUrl(addon_handle, True, listitem)
+    addLink("Testvideo" , url="rtmp://artestras.fc.llnwd.net/artestras app=artestras swfUrl=http://www.arte.tv/arte_vp/jwplayer6/jwplayer.flash.6.11.4923.swf pageURL=http://www.arte.tv/guide/de/live", mode="playlive", iconimage="",duration="",desc="")    
+    xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
+
+def parameters_string_to_dict(parameters):
+	paramDict = {}
+	if parameters:
+		paramPairs = parameters[1:].split("&")
+		for paramsPair in paramPairs:
+			paramSplits = paramsPair.split('=')
+			if (len(paramSplits)) == 2:
+				paramDict[paramSplits[0]] = paramSplits[1]
+	return paramDict
+  
+   
+
+def getUrl(url, cookie=None):
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:25.0) Gecko/20100101 Firefox/25.0')
+    if cookie != None:
+        req.add_header('Cookie', cookie)
+    response = urllib2.urlopen(req)
+    link = response.read()
+    response.close()
+    return link
+
+       
+params = parameters_string_to_dict(sys.argv[2])
+mode = urllib.unquote_plus(params.get('mode', ''))
+url = urllib.unquote_plus(params.get('url', ''))
+name = urllib.unquote_plus(params.get('name', ''))
+showName = urllib.unquote_plus(params.get('showName', ''))
+hideShowName = urllib.unquote_plus(params.get('hideshowname', '')) == 'True'
+nextPage = urllib.unquote_plus(params.get('nextpage', '')) == 'True'
+einsLike = urllib.unquote_plus(params.get('einslike', '')) == 'True'    
+
+# Haupt Menu Anzeigen      
+if mode is '':    
+    addLink("Testvideo" , url="-", mode="playlive", iconimage="",duration="",desc="")    
+    xbmcplugin.endOfDirectory(addon_handle,succeeded=True,updateListing=False,cacheToDisc=True)
+else:  
+  if mode == 'playlive':
+          playlive(url)     
