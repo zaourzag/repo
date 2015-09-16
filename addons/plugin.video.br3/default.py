072e75bead3176be2ce2e5f112e84b2b7ecafb58
root <root@vogler.name>
2015-09-16 21:45:14 +0200

    Fehler korrigiert

--- addons/plugin.video.br3/default.py
+++ addons/plugin.video.br3/default.py
@@ -188,7 +188,7 @@ def jsonurl(url) :
         debug("dauer:"+str(dauer))
       if not start_datum=="":
          if starttime <  nowtime  and  endtime >  nowtime() :  
-          dauer=str((endtime-nowtime)/60)
+           dauer=str((endtime-nowtime)/60)
            title=title +" ( [COLOR red] Läuft [/COLOR])"
          else:
            title=title +" ( "+  time.strftime("%d/%m/%Y %H:%M",startobj)  +" Uhr)"
