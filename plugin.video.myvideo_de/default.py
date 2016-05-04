a3e502a224390d1bb73a1ffd9335fc59b68a0f8b
root <root@vogler.name>
2016-05-04 15:17:12 +0200

    Myvideo ist bageschaltet worden

--- plugin.video.myvideo_de/default.py
+++
@@ -1,5 +0,0 @@
-from resources.lib import kodimon
-from resources.lib import myvideo
-
-__provider__ = myvideo.Provider()
-kodimon.run(__provider__)
--- plugin.video.myvideo_de/resources/lib/kodimon/input.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'L0RE'
-
-try:
-    from .impl.xbmc.xbmc_input import *
-except ImportError:
-    from .impl.mock.mock_input import *
-    pass
