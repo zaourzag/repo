bdefcc9a2a3ddb640d34d0a071d66d4ead634976
SLiX69 <asd2304@gmail.com>
2017-03-07 21:28:41 +0100

    remove plugin.program.jdownlaoder2

--- plugin.program.jdownloader2/resources/lib/dialogs.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import xbmcgui
-
-
-def put_notification(head, line, dura=3):
-    # TODO icon
-    dialog = xbmcgui.Dialog()
-    dialog.notification(head, line, xbmcgui.NOTIFICATION_INFO, dura)
