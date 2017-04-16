eb06bb88255809d0ea8dbdb9858ad5214ba1c048
tyl0re <andreas@vogler.name>
2017-04-17 01:18:58 +0200

    Update

--- plugin.video.fernsehkritik_tv/default.py
+++ plugin.video.fernsehkritik_tv/default.py
@@ -10,6 +10,7 @@ import random
 import xbmcplugin
 import xbmcgui
 import xbmcaddon
+import json
 
 addonID = 'plugin.video.fernsehkritik_tv'
 addon = xbmcaddon.Addon(id=addonID)
@@ -34,51 +35,60 @@ def main():
 	else:
 		addDir('meine Downloads','','listing',icon)
 	xbmcplugin.endOfDirectory(pluginhandle)
-
+def folge(url,page="1"):    
+    anzahl=100
+    content = opener.open(url).read()
+    PID = re.compile('MAG_PID = ([0-9]+?);', re.DOTALL).findall(content)[0]
+    SID = re.compile('MAG_SPID = ([0-9]+?);', re.DOTALL).findall(content)[0]   
+    print ("### "+url)
+    print ("PID "+PID)
+    print ("SID "+SID)
+    nu="https://massengeschmack.tv/api/api_p.php?action=getFeed&from=["+str(PID)+"]&access_type=0&limit="+str(anzahl)+"&contentType=["+str(SID)+"]&page="+str(page)
+    print("NU :"+nu)
+    content = opener.open(nu).read()
+    struktur = json.loads(str(content))
+    x=1
+    for element in struktur["eps"]:
+      x=x+1
+      uurl="https://massengeschmack.tv/play/"+element["identifier"]
+      title=element["title"]
+      icon=element["img"]
+      print("###----")
+      print(element["subscribed"])
+      if element["subscribed"]==True:
+        addLink(title,uurl,'playVideo',icon,'','')
+    if x>=anzahl:
+       addDir("Next",url,"folge","",page=int(page)+1)
+    xbmcplugin.endOfDirectory(pluginhandle)
+    
 def index(url):
     content = opener.open(url).read()
-    pag = re.compile('<li class="active"><a href=".*?">\d+</a>.*?<li class=""><a href="(.*?)">\d+</a></li>', re.DOTALL).findall(content)
-    spl = content.split('<div class="newitem">')
-    for i in range(1, len(spl), 1):
-        entry = spl[i]
-        match = re.compile('<h4><a href="(.+?)">(.+?)</a>', re.DOTALL).findall(entry)
-        url = urlMain+match[0][0]
-        title = match[0][1].replace('&quot;','')
-        match = re.compile('<img.*?src="(.*?jpg)"', re.DOTALL).findall(entry)
-        thumb = match[0]
-        thumb = thumb.replace('../', '/')
-        match=re.compile('<p>(.*?)<strong>(.*?)</strong>.</p>', re.DOTALL).findall(entry)
-        desc=match[0][0].replace('&quot;','')
-        time=match[0][1]
-        addLink(title, url, "playVideo", thumb,desc)
-    if pag:
-		try:
-			url='http://fernsehkritik.tv/tv-magazin/'+pag[1]
-			addDir('NEXT',url,"index",icon)
-		except:
-			url='http://fernsehkritik.tv/tv-magazin/'+pag[0]
-			addDir('NEXT',url,"index",icon)
+    pag = re.compile('<li><a href="(.+?)">(.+?)</a></li>', re.DOTALL).findall(content)
+    for url,name in pag:
+       url="https://massengeschmack.tv"+url          
+       addDir(name,url,"folge","",page=1)		
     xbmcplugin.endOfDirectory(pluginhandle)
     xbmc.executebuiltin('Container.SetViewMode(%d)' % view_mode_id)
 	
-def getVideourl(url):
+def getVideourl(url):    
 	content = opener.open(url).read()		
 	if re.findall('<h4>Clip nicht kostenlos verfügbar.</h4>',content,re.S):
 		print 'video nicht verfügbar'
 		xbmc.executebuiltin('XBMC.Notification(Info:,Folge ist noch nicht verfügbar!,5000,'+icon+')')
 	else:
 		print 'weiter'
-		match=re.findall('type: "video/mp4",src:  "([^"]+)"',content,re.S)
+		match=re.findall('type: "video/mp4",.+?src: "([^"]+)"',content,re.S)
 		if match:
 			video_url=match[0]
 			return video_url
 			
  
 def playVideo(url):
-	url=url+'play/'
-	video_url=getVideourl(url)
-	listitem=xbmcgui.ListItem(path=video_url)
-	xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
+  print ("#########"+url)
+  video_url=getVideourl(url)
+  print ("###########"+video_url)
+  listitem=xbmcgui.ListItem(path=video_url)
+  xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
 	
 def download(url):
 	if downloadpath is '':
@@ -144,8 +154,8 @@ def addLink(name, url, mode, iconimage, desc="", duration=""):
     ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz)
     return ok
 
-def addDir(name,url,mode,iconimage):
-    u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+def addDir(name,url,mode,iconimage,page=1):
+    u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&page="+str(page)
     ok=True
     item=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
     item.setInfo( type="Video", infoLabels={ "Title": name } )
@@ -157,6 +167,7 @@ mode = urllib.unquote_plus(params.get('mode', ''))
 url = urllib.unquote_plus(params.get('url', ''))
 name = urllib.unquote_plus(params.get('name', ''))
 thumb = urllib.unquote_plus(params.get('thumb', ''))
+page = urllib.unquote_plus(params.get('page', ''))
 
 if mode == 'index':
 	index(url)
@@ -168,6 +179,8 @@ elif mode =='listing':
 	listing()
 elif mode =='play1':
 	play1(url)
+elif mode =='folge':
+	folge(url,page)  
 elif mode =='59':
     download(url)
 else:
