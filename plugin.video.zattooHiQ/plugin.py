f4bd4ab236741fe178e45c14da18a6e58f91375b
www-data <www-data@vogler.name>
2017-06-18 13:41:32 +0200

    Autoupdate

--- plugin.video.zattooHiQ/plugin.py
+++ plugin.video.zattooHiQ/plugin.py
@@ -229,7 +229,7 @@ def build_recordingsList(addon_uri, addon_handle):
   import urllib
   
   resultData = _zattooDB_.zapi.exec_zapiCall('/zapi/playlist', None)
-  print 'resultData ' + str(resultData)
+ # print 'resultData ' + str(resultData)
   if resultData is None: return
   
   for record in resultData['recordings']:
@@ -298,12 +298,9 @@ def build_recordingsList(addon_uri, addon_handle):
   xbmcplugin.addSortMethod(addon_handle, 2)
   xbmcplugin.addSortMethod(addon_handle, 9)
 
-def watch_recording(addon_uri, addon_handle, recording_id, start):
+def watch_recording(addon_uri, addon_handle, recording_id):
   #if xbmc.Player().isPlaying(): return
-  print 'Starttime ' + str(startTime)
-  startTime = int(start)
-  if startTime is None:
-    startTime=int(xbmc.getInfoLabel('ListItem.Property(zStartTime)'))
+  startTime=int(xbmc.getInfoLabel('ListItem.Property(zStartTime)'))
  
   max_bandwidth = __addon__.getSetting('max_bandwidth')
   if DASH: stream_type='dash'
@@ -1052,8 +1049,7 @@ def main():
     xbmc.executebuiltin('Container.Refresh')
   elif action == 'watch_r':
     recording_id = args.get('id')[0]
-    startTime = args.get('startTime',[None])[0]
-    watch_recording(addon_uri, addon_handle, recording_id, startTime)
+    watch_recording(addon_uri, addon_handle, recording_id)
   elif action == 'record_p':
     program_id = args.get('program_id')[0]
     setup_recording(program_id)
