eabbc1f8cb9685f0b7eaf7b87e2fff69b647a2b3
tyl0re <andreas@vogler.name>
2017-09-06 13:35:00 +0200

    Update

--- plugin.video.4playersweb/addon.py
+++ plugin.video.4playersweb/addon.py
@@ -22,6 +22,52 @@ defaultBackground = ""
 icon = xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('path')+'/icon.png').decode('utf-8')
 addon_handle = int(sys.argv[1])
 
+
+type=addon.getSetting("Type") 
+if  type=="Alle Systeme":
+    type="videos"
+if type=="PC":
+   type="PC-CDROM"
+if type=="XBox One":
+   type=="XboxOne"
+if type=="Playstation 4":
+   type="PlayStation4"
+if type=="XBox 360":
+   type="PlayStation4"
+if type=="360":
+   type="PlayStation3"
+if type=="Switch":
+   type="Switch"
+if type=="Wii U":
+   type="Wii_U"   
+if type=="Select":
+   type="-1"       
+newtype=addon.getSetting("NEWType") 
+if  newtype=="Alle":
+    newtype=""
+if newtype=="Top":
+   newtype="/TopRated/Top"
+if newtype=="Meistgesehen":
+   newtype=="/TopViews/Meistgesehen"
+if newtype=="PC":
+   newtype="/PC-CDROM/PC"
+if newtype=="One":
+   newtype="/XboxOne/One"
+if newtype=="PS4P":
+   newtype="/PlayStation4/PS4"
+if newtype=="Switch":
+   newtype="/Switch/Switch"
+if newtype=="Handhelds":
+   newtype="/Handhelds/Handhelds"   
+if newtype=="PSVR":
+   newtype="/PlayStationVR/PSVR"
+if newtype=="PS4P":
+   newtype="/PlayStation4Pro/Alle"   
+if newtype=="PC-VR":
+   newtype="/PC-VR/PC-VR"     
+if newtype=="Select":
+   newtype="-1"       
+
 if not xbmcvfs.exists(temp):       
        xbmcvfs.mkdirs(temp)
        
@@ -37,8 +83,9 @@ def log(msg, level=xbmc.LOGNOTICE):
     addonID = addon.getAddonInfo('id')
     xbmc.log('%s: %s' % (addonID, msg), level) 
     
-def addDir(name, url, mode, iconimage, desc="",filter=""):
-  u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&filter="+str(filter)
+def addDir(name, url, mode, iconimage, desc="",ffilter=""):
+  u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&ffilter="+urllib.quote_plus(str(ffilter))+"&mode="+str(mode)
+  debug(" U :"+u)
   liz = xbmcgui.ListItem(name, iconImage=icon, thumbnailImage=iconimage)
   liz.setInfo(type="Video", infoLabels={"Title": name, "Plot": desc})  
   liz.setArt({ 'fanart': defaultBackground })    
@@ -88,38 +135,69 @@ def geturl(url,header=""):
 params = parameters_string_to_dict(sys.argv[2])
 mode = urllib.unquote_plus(params.get('mode', ''))
 url = urllib.unquote_plus(params.get('url', ''))
-filter = urllib.unquote_plus(params.get('filter', ''))
+ffilter = urllib.unquote_plus(params.get('ffilter', ''))
+debug("## F :"+sys.argv[2])
+
 
 def index():
   content=geturl("http://www.4players.de/4players.php/browsevideostv/videos/")
   htmlPage = BeautifulSoup(content, 'html.parser')
   element = htmlPage.find("select",attrs={'name':'FILTER'})
   spalten = element.find_all('option')
-  for spalte in spalten:        
-   addDir(spalte.text, spalte["value"], 'listrubriken', "")   
+  addDir("Settings", "", 'Settings', "")
+  if not newtype=="-1":
+    addDir("Neueste Videos", "http://www.4players.de/4players.php/browsevideostv###/Trailer_Videos_TV.html#pagetop", 'listrubrik', "",ffilter=newtype)   
+  else:     
+    addDir("Neueste Videos", "http://www.4players.de/4players.php/browsevideostv###/Trailer_Videos_TV.html#pagetop", 'newvideos', "",ffilter=newtype)     
+  debug("TYPE :"+type)
+  for spalte in spalten: 
+   if type=="-1":       
+      addDir(spalte.text, spalte["value"], 'listrubriken', "")   
+   else:
+       addDir(spalte.text, spalte["value"], 'listrubrik', "",ffilter=type)   
   xbmcplugin.endOfDirectory(addon_handle,succeeded=True,updateListing=False,cacheToDisc=True)   
 
+  
+def newvideos(url,ffilter):
+    addDir("Alle", url, 'listrubrik', "",ffilter="")  
+    addDir("Top", url, 'listrubrik', "",ffilter="/TopRated/Top")  
+    addDir("Meistgesehen", url, 'listrubrik', "",ffilter="/TopViews/Meistgesehen")  
+    addDir("PC", url, 'listrubrik', "",ffilter="/PC-CDROM/PC")  
+    addDir("One", url, 'listrubrik',"", ffilter="/XboxOne/One")  
+    addDir("PS4", url, 'listrubrik', "",ffilter="/PlayStation4/PS4")  
+    addDir("PSVR", url, 'listrubrik', "",ffilter="/PlayStationVR/PSVR")  
+    addDir("PS4P", url, 'listrubrik',"", ffilter="/PlayStation4Pro/Alle")  
+    addDir("PC-VR", url, 'listrubrik', "",ffilter="/PC-VR/PC-VR")  
+    addDir("Switch", url, 'listrubrik', "",ffilter="/Switch/Switch")  
+    addDir("Handhelds", url, 'listrubrik',"",ffilter= "/Handhelds/Handhelds")  
+    xbmcplugin.endOfDirectory(addon_handle,succeeded=True,updateListing=False,cacheToDisc=True)   
+   
+  
 def listrubriken(url):
+   debug("listrubriken url :"+url)
    content=geturl("http://www.4players.de/4players.php/browsevideostv/videos/"+url+"/")
    htmlPage = BeautifulSoup(content, 'html.parser')
    element = htmlPage.find("select",attrs={'name':'SYSTEM'})
    spalten = element.find_all('option')
    for spalte in spalten:        
-      addDir(spalte.text, url, 'listrubrik', "",filter=spalte["value"])   
+      addDir(spalte.text, url, 'listrubrik', "",ffilter=spalte["value"])
+      debug(spalte["value"])
    xbmcplugin.endOfDirectory(addon_handle,succeeded=True,updateListing=False,cacheToDisc=True)      
-def listrubrik(url,filter)   :
+def listrubrik(url,ffilter)   :
   if not "http:" in url:
-    geturll=  "http://www.4players.de/4players.php/browsevideostv/videos"+filter+"/"+url+"/Trailer_Videos_TV.html"
+    geturll=  "http://www.4players.de/4players.php/browsevideostv/"+ffilter+"/"+url+"/Trailer_Videos_TV.html"
   else:
-     geturll=url
+     geturll=url.replace("###",ffilter)
+  debug("geturll :"+geturll)
+  debug("url :"+url)
+  debug("ffilter :"+ffilter)
   content=geturl(geturll)
   htmlPage = BeautifulSoup(content, 'html.parser')
   elemente = htmlPage.find_all("div",attrs={'class':'tv-aktuell-box index-system-hauptthema'})
-  for element in elemente:
-    debug("------")
-    debug(element)
+  for element in elemente:        
     link=element.find("a")["href"]
     img=element.find("div",attrs={'class':'skim'})["data-skimimageurl"].replace("skimimage.jpg","thumb300x168.jpg")
+    debug("IMG :"+img)
     title=element.find("a")["title"]
     spiel=element.find("div",attrs={'class':'spielname left'}).text
     beschreibung=element.find("div",attrs={'class':'einleitung'}).text.strip()
@@ -135,7 +213,8 @@ def listrubrik(url,filter)   :
   except:
     pass  
   xbmcplugin.endOfDirectory(addon_handle,succeeded=True,updateListing=False,cacheToDisc=True) 
-
+ 
+  
 def playvideo(url):
   content=geturl(url)
   newlink=re.compile("playlist:[^']+'(.+?)'", re.DOTALL).findall(content)[0]
@@ -160,13 +239,17 @@ def playvideo(url):
     
 
 
-
-     
+  
 if mode=="":
     index()      
 if mode=="listrubriken":
    listrubriken(url)
 if mode=="listrubrik":
-   listrubrik(url,filter)
+   listrubrik(url,ffilter)
 if mode=="playvideo":
    playvideo(url)   
+if mode == 'Settings':
+          addon.openSettings()       
+if mode=="newvideos":
+    newvideos(url,ffilter)
+    
