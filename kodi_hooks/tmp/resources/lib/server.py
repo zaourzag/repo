1c7bd61cc0da0de35488182ba31d0476f021a979
tyl0re <andreas@vogler.name>
2017-10-04 16:55:53 +0200

    Autoupdate

---
+++ kodi_hooks/tmp/resources/lib/server.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# Akibapass - Watch videos from the german anime platform Akibapass.de on Kodi.
+# Copyright (C) 2016 - 2017 MrKrabat
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import time
+import socket
+import threading
+
+import xbmc
+
+
+header = "HTTP/1.1 200 OK\nContent-Type: application/vnd.apple.mpegurl; charset=utf-8\n\n"
+
+def streamprovider(file):
+    """Server returning manifest to kodi
+    """
+    t = threading.currentThread()
+    try:
+        # create listening socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(("127.0.0.1", 10147))
+        sock.listen(1)
+        sock.setblocking(0)
+    except socket.error:
+        # unable to listen on port
+        xbmc.log("[SERVICE] Akibapass: Failed listening on port 10147", xbmc.LOGFATAL)
+        return
+
+    while getattr(t, "do_run", True):
+        time.sleep(0.05)
+        try:
+            # wait for connection
+            connection, client_address = sock.accept()
+            try:
+                # wait for request
+                connection.settimeout(10.0)
+                data = connection.recv(4096).rstrip()
+
+                # send m3u8
+                connection.sendall(header + file)
+            finally:
+                # close connection
+                connection.close()
+        except socket.error:
+            # continue if no connection
+            continue
+
+    # close socket
+    sock.close()
