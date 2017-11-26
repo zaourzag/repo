7f55003f085b8903fc6c25deb1ddfeded1b1d755
rolapp <github@rolapp.de>
2017-11-26 19:38:41 +0100

    initial commit

---
+++ service.smartdns/service.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+#
+#
+# 	 smartdns
+# 	(c) 2017 Steffen Rolapp
+
+#
+
+import xbmc, xbmcgui, xbmcplugin, xbmcaddon
+import os, re, base64
+import urllib, urllib2
+
+__addon__ = xbmcaddon.Addon()
+__addonId__=__addon__.getAddonInfo('id')
+__addonname__ = __addon__.getAddonInfo('name')
+
+URL = __addon__.getSetting('dnsurl')
+TESTIP = 'http://ifconfig.co/ip'
+OLDIP = "0"
+
+def testip(IP):
+    OLDIP = IP
+    try:
+        handle = urllib2.urlopen(TESTIP)
+        newip = handle.read()
+    except:
+        newip = OLDIP
+   
+    if  OLDIP != newip:
+        OLDIP = newip
+        handle = urllib2.urlopen(URL)
+        html = handle.read()
+        #html = 'test'
+        xbmcgui.Dialog().notification('SmartDNS', str(html) + " " + str(newip),   __addon__.getAddonInfo('path') + '/icon.jpg', 3000, False)
+    return OLDIP
+        
+def timer(OLDIP):
+    monitor = xbmc.Monitor()
+    while not monitor.abortRequested():
+        if monitor.waitForAbort(300): break
+        OLDIP = testip(OLDIP)
+        #print 'OLDIP ' + str(OLDIP)
+	
+OLDIP = testip(OLDIP)
+timer(OLDIP)
