ebd86919e63e56ef6f5e681f217080ee7396fe39
tyl0re <andreas@vogler.name>
2017-12-08 22:45:20 +0100

    Update

---
+++ plugin.video.L0RE.disneychannel/default.py
@@ -0,0 +1,194 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import urlparse
+import xbmcgui
+import xbmcplugin
+import xbmcaddon
+import xbmc
+import xbmcvfs
+import urllib, urllib2, socket, cookielib, re, os, shutil,json
+import time
+import base64
+import requests
+from bs4 import BeautifulSoup
+from HTMLParser import HTMLParser
+from django.utils.encoding import smart_str
+
+
+# Setting Variablen Des Plugins
+global debuging
+base_url = sys.argv[0]
+addon_handle = int(sys.argv[1])
+args = urlparse.parse_qs(sys.argv[2][1:])
+addon = xbmcaddon.Addon()
+# Lade Sprach Variablen
+translation = addon.getLocalizedString
+
+
+xbmcplugin.setContent(addon_handle, 'movies')
+
+baseurl="http://tv.disney.de"
+xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_UNSORTED)
+xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_VIDEO_SORT_TITLE)
+
+
+icon = xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('path')+'/icon.png').decode('utf-8')
+
+
+profile    = xbmc.translatePath( addon.getAddonInfo('profile') ).decode("utf-8")
+temp       = xbmc.translatePath( os.path.join( profile, 'temp', '') ).decode("utf-8")
+
+if xbmcvfs.exists(temp):
+  shutil.rmtree(temp)
+xbmcvfs.mkdirs(temp)
+cookie=os.path.join( temp, 'cookie.jar')
+cj = cookielib.LWPCookieJar();
+
+if xbmcvfs.exists(cookie):
+    cj.load(cookie,ignore_discard=True, ignore_expires=True)                  
+
+def debug(content):
+    log(content, xbmc.LOGDEBUG)
+    
+def notice(content):
+    log(content, xbmc.LOGNOTICE)
+
+def log(msg, level=xbmc.LOGNOTICE):
+    addon = xbmcaddon.Addon()
+    addonID = addon.getAddonInfo('id')
+    xbmc.log('%s: %s' % (addonID, msg), level) 
+    
+
+  
+def addDir(name, url, mode, thump, desc="",page=1,nosub=0):   
+  u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&page="+str(page)+"&nosub="+str(nosub)
+  ok = True
+  liz = xbmcgui.ListItem(name)  
+  liz.setArt({ 'fanart' : thump })
+  liz.setArt({ 'thumb' : thump })
+  liz.setArt({ 'banner' : icon })
+  liz.setArt({ 'fanart' : icon })
+  liz.setInfo(type="Video", infoLabels={"Title": name, "Plot": desc})
+	
+  ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
+  return ok
+  
+def addLink(name, url, mode, thump, duration="", desc="", genre='',director="",bewertung=""):
+  debug("URL ADDLINK :"+url)
+  debug( icon  )
+  u = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)
+  ok = True
+  liz = xbmcgui.ListItem(name,thumbnailImage=thump)
+  liz.setArt({ 'fanart' : icon })
+  liz.setInfo(type="Video", infoLabels={"Title": name, "Plot": desc, "Genre": genre, "Director":director,"Rating":bewertung})
+  liz.setProperty('IsPlayable', 'true')
+  liz.addStreamInfo('video', { 'duration' : duration })
+	#xbmcplugin.setContent(int(sys.argv[1]), 'tvshows')
+  ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz)
+  return ok
+  
+def parameters_string_to_dict(parameters):
+	paramDict = {}
+	if parameters:
+		paramPairs = parameters[1:].split("&")
+		for paramsPair in paramPairs:
+			paramSplits = paramsPair.split('=')
+			if (len(paramSplits)) == 2:
+				paramDict[paramSplits[0]] = paramSplits[1]
+	return paramDict
+
+
+def geturl(url,data="x",header="",referer=""):
+        global cj
+        debug("Get Url: " +url)
+        for cook in cj:
+          debug(" Cookie :"+ str(cook))
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))        
+        userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"
+        if header=="":
+          opener.addheaders = [('User-Agent', userAgent)]        
+        else:
+          opener.addheaders = header        
+        if not referer=="":
+           opener.addheaders = [('Referer', referer)]
+
+        try:
+          if data!="x" :
+             content=opener.open(url,data=data).read()
+          else:
+             content=opener.open(url).read()
+        except urllib2.HTTPError as e:
+             #debug( e.code )  
+             cc=e.read()  
+             debug("Error : " +cc)
+             content=""
+       
+        opener.close()
+        cj.save(cookie,ignore_discard=True, ignore_expires=True)               
+        return content
+
+
+  
+def liste():
+    content=geturl(baseurl+"/disney-channel")
+    jsondata=re.compile('<script type="text/javascript">this.Grill\?Grill.burger=(.+?):\(function\(\)', re.DOTALL).findall(content)[0]   
+    jsondata=smart_str(jsondata)
+    debug(jsondata)
+    struct = json.loads(jsondata)   
+    for element in struct["stack"]:
+       debug(element)
+       if element["view"]=="stream":
+         debug("Stream")
+         for video in element["data"]:
+           title=video["title"].encode("utf-8")
+           thump=video["thumb"]
+           link=video["href"]
+           addLink(title, link, "playvideo",thump)          
+    xbmcplugin.endOfDirectory(addon_handle) 
+
+  
+def playvideo(url): 
+    debug("URL :"+url)        
+    content=geturl(url)
+    jsondata=re.compile('<script type="text/javascript">this.Grill\?Grill.burger=(.+?):\(function\(\)', re.DOTALL).findall(content)[0]   
+    jsondata=smart_str(jsondata)
+    struct = json.loads(jsondata)   
+    debug("--------")
+    debug(jsondata)
+    videourl=""
+    for element in struct["stack"]:
+        if element["view"]=="watch_3":
+           folge=element["data"][0]
+           title=folge["title"]
+           videourl=folge["embedURL"]
+    content=geturl(videourl)   
+    jsondata=re.compile('"hls_only_android","tokenization"],"url":"(.+?)"', re.DOTALL).findall(content)
+    debug("+++++")
+    debug(jsondata)
+    video=jsondata[-1]
+    listitem = xbmcgui.ListItem(path=video)
+    xbmcplugin.setResolvedUrl(addon_handle, True, listitem)
+    
+params = parameters_string_to_dict(sys.argv[2])
+mode = urllib.unquote_plus(params.get('mode', ''))
+url = urllib.unquote_plus(params.get('url', ''))
+referer = urllib.unquote_plus(params.get('referer', ''))
+page = urllib.unquote_plus(params.get('page', ''))
+nosub= urllib.unquote_plus(params.get('nosub', ''))
+
+     
+if mode is '':
+     liste()   
+else:
+  # Wenn Settings ausgewählt wurde
+  if mode == 'Settings':
+          addon.openSettings()
+  # Wenn Kategory ausgewählt wurde
+  if mode == 'playvideo':
+          playvideo(url)
+  if mode == 'subrubrik':
+          subrubrik(url)
+  if mode == 'videoliste':
+          videoliste(url,page,nosub)
