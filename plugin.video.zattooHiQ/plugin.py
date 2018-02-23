606edfe062cc0c1010195c242b807fc771518b96
rolapp <github@rolapp.de>
2018-02-23 19:11:07 +0100

    v1.4.2

--- plugin.video.zattooHiQ/plugin.py
+++ plugin.video.zattooHiQ/plugin.py
@@ -20,7 +20,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with zattooHiQ.  If not, see <http://www.gnu.org/licenses/>.
 #
-
+#
 
 REMOTE_DBG = False
 if REMOTE_DBG:
