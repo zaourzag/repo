f5fb305d3b3f2b16373a3201abcaa34f785439e3
tyl0re <andreas@vogler.name>
2018-03-22 18:07:04 +0100

    Update

--- plugin.video.L0RE.kinder_mediatheken/default.py
+++ plugin.video.L0RE.kinder_mediatheken/default.py
@@ -7,6 +7,7 @@ addon_handle = int(sys.argv[1])
 addonID = addon.getAddonInfo('id')
 json_addons='{"elements":[{"name":"Disney","id":"plugin.video.L0RE.disneychannel"},{"name":"Nickolodion","id":"plugin.video.nick_de"},{"id":"plugin.video.L0RE.kividoo","name":"Kividoo"}'
 json_addons=json_addons+',{"id":"plugin.video.kikamediathek","name":"Kika Mediathek"},{"id":"plugin.video.funkmediathek","name":"Funk Mediathek"},{"id":"plugin.video.sesamstrasse_de","name":"Sesamstrasse"}'
+json_addons=json_addons+',{"id":"plugin.video.zdftivi","name":"ZDF Tivi"},{"id":"plugin.video.kika_de","name":"KIKA+"}
 json_addons=json_addons+']}'
 print(json_addons)
 struktur = json.loads(json_addons)  
