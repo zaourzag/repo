7e6ae09c0502b66105202a4801693bc670a135b4
tyl0re <andreas@vogler.name>
2018-06-16 14:37:15 +0200

    plugin.video.wm2018dach

--- plugin.video.wm2018dach/resources/lib/wm2018.py
+++ plugin.video.wm2018dach/resources/lib/wm2018.py
@@ -66,29 +66,30 @@ def check_mode(args):
 def showMainMenue(args):
     """Show main menu
     """
-    # football matches
-    matches = {"0": "https://wm2018.akamaized.net/hls/live/625036/wm2018/master.m3u8?b=0-", # ARD
-               "1": "https://zdf1314-lh.akamaihd.net/i/de14_v1@392878/master.m3u8?", # ZDF
-               "2": "http://orf1ad.orf.cdn.ors.at/out/u/orf1ad/qxb/manifest.m3u8", # ORF
-               "3": "http://ors-uhd.cdn.ors.at/out/u/orfuhd-abr-hdr10_1.m3u8"} # ORF UHD
+    # fix UHD removed
+    if args._addon.getSetting("stream") == "3":
+        args._addon.setSetting("stream", "2")
 
-    # press conference
-    conference = {"0": "https://wdrardevent1-lh.akamaihd.net/i/ardevent1_weltweit@566648/master.m3u8?b=0-", # ARD
-                  "1": "https://zdf0506-lh.akamaihd.net/i/de06_v1@392858/master.m3u8?", # ZDF
-                  "2": False, # ORF
-                  "3": False} # ORF UHD
+    # stream files
+    streams = {"0": {"Fußballspiel live":    "https://wm2018.akamaized.net/hls/live/625036/wm2018/master.m3u8?b=0-", # ARD
+                     "Pressekonferenz live": "https://wdrardevent1-lh.akamaihd.net/i/ardevent1_weltweit@566648/master.m3u8?b=0-"},
+
+               "1": {"Fußballspiel live":    "https://zdf1314-lh.akamaihd.net/i/de14_v1@392878/master.m3u8?", # ZDF
+                     "Draufsicht 1 live":    "http://zdf0304-lh.akamaihd.net/i/de03_v1@392855/master.m3u8",
+                     "Draufsicht 2 live":    "http://zdf0506-lh.akamaihd.net/i/de05_v1@392857/master.m3u8",
+                     "Trainersicht live":    "http://zdf0304-lh.akamaihd.net/i/de04_v1@392856/master.m3u8",
+                     "Pressekonferenz live": "https://zdf0506-lh.akamaihd.net/i/de06_v1@392858/master.m3u8?"},
+
+               "2": {"Adaptiv Fußballspiel live": "http://orf1ad.orf.cdn.ors.at/out/u/orf1ad/qxb/manifest.m3u8", # ORF
+                     "4K Fußballspiel live":      "http://ors-uhd.cdn.ors.at/out/u/orfuhd-abr-hdr10_1.m3u8",
+                     "2K Fußballspiel live":      "http://ors-uhd.cdn.ors.at/out/u/orfuhd-abr-hdr10_2.m3u8"}}
 
 
-    if matches[args._addon.getSetting("stream")]:
+    # create directory
+    for key, value in list(streams[args._addon.getSetting("stream")].items()):
         view.add_item(args,
-                      {"title": args._addon.getLocalizedString(30040),
-                       "url":   matches[args._addon.getSetting("stream")],
-                       "mode":  "videoplay"},
-                      isFolder=False)
-    if conference[args._addon.getSetting("stream")]:
-        view.add_item(args,
-                      {"title": args._addon.getLocalizedString(30041),
-                       "url":   conference[args._addon.getSetting("stream")],
+                      {"title": key,
+                       "url":   value,
                        "mode":  "videoplay"},
                       isFolder=False)
 
