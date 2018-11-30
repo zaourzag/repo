e5a9bccbc27eda429f9c2fee24e8b4b8bfc3f164
AlleSchonWeg <6727366+AlleSchonWeg@users.noreply.github.com>
2018-11-30 10:44:47 +0100

    Fix Unix LF

---
+++ plugin.video.skygo.de/common.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib
+import xbmcaddon
+
+base_url = "plugin://" + xbmcaddon.Addon().getAddonInfo('id')
+
+
+def build_url(query):
+    return base_url + '?' + urllib.urlencode(query)
