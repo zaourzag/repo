0d9d07817a8d50ebc7b5146c478bbde6a2632550
Collabsvito <armishath88@gmail.com>
2019-03-12 22:45:06 +0100

    plugin.video.dfb_tv-2.0.4
    - fixed: Wieder werden Livestreams NICHT abgespielt (Anfrage für Live- und Standardstreams im Script getrennt)

--- plugin.video.dfb_tv/default.py
+++ plugin.video.dfb_tv/default.py
@@ -110,25 +110,87 @@ def index():
 	add_category(title = translation(30609), mode = 'search')
 	xbmcplugin.endOfDirectory(pluginhandle)
 
-def get_last_page(total_results):
-	if not total_results: return 0
-	if total_results % 10 == 0: return total_results // 10
-	return total_results // 10 + 1
-
-def clean_text(text):
-	return text.replace('\n', '').replace('\t', '').replace('&amp;', '&').replace('   ', '').replace('  ', ' ').replace('\\"', '"').replace('\\u0026', '&').replace('\\t', '').replace(': :', ':').strip()
-
-def list_all_livestreams(live_url = 'https://tv.dfb.de/static/livestreams/'):
-	debug("(list_all_livestreams) -------------------------------------------------- START = list_all_livestreams --------------------------------------------------")
+def list_all_videos(search_string = '', categories = '', current_page = 1, last_page = 0, meta = ''):
+	debug("(list_all_videos) -------------------------------------------------- START = list_all_videos --------------------------------------------------")
+	json_url = 'http://search.dfb.de/search/videos.json?'
+	if search_string: json_url += 'q=' + search_string + '&'
+	if categories: json_url += 'categories=' + categories + '&'
+	if meta: json_url += 'meta_attribute_ids' + meta + '&'
+	json_url += 'page=' + str(current_page)
+	debug("(list_all_videos) ### Json_url : "+json_url+" ###")
 	try:
-		html = getUrl(live_url)
-		regex_videos = '<div class="video-teaser.*?<img src=".*?/images/(.+?)_.*?subline">(.+?)</.*?-headline">(.+?)</'
-		videos = re.findall(regex_videos, html, re.DOTALL)
+		json = getUrl(json_url)
+		if HD: regex_video_info = '"guid":.*?video-(.+?)","title":"(.+?)",.*?"medium_hires":"(.+?)",.*?"pub_date":"(.+?)"'
+		else: regex_video_info = '"guid":.*?video-(.+?)","title":"(.+?)",.*?"image_url":"(.+?)",.*?"pub_date":"(.+?)"'
+		videos = re.findall(regex_video_info, json, re.DOTALL)
 	except: return False
 	item_added = False
 	for video in videos:
 		try:
-			video_id = video[0]
+			video_id = video[0].strip()
+			title = clean_text(video[1])
+			if HD: thumb = video[2].replace('medium_hires', 'large_hires')
+			else: thumb = video[2].replace('medium?', 'medium_hires?')
+			date = video[3].strip()
+			add_video(date+' - '+title, video_id, thumb, 'play-video')
+			debug("(list_all_videos) ### Title : "+str(date)+' - '+title+" ###")
+			debug("(list_all_videos) ### Video_id : "+str(video_id)+" ###")
+			debug("(list_all_videos) ### Thumb : "+thumb+" ###")
+			item_added = True
+		except:
+			failing("(list_all_videos) ERROR - ERROR - ERROR ### {0} ###".format(str(video)))
+	if not item_added: return warning(translation(30523))
+	if not last_page: 
+		regex_total = '"total":(.+?),'
+		try: last_page = get_last_page(int(re.findall(regex_total, json)[0]))
+		except: last_page = current_page
+		debug("(list_all_videos) ### Last_page : "+str(last_page)+" ###")
+	if current_page < last_page:
+		debug("(list_all_videos) ### Current_page : "+str(current_page)+" ### Last_page : "+str(last_page)+" ###")
+		add_category((translation(30610).format(current_page+1)), 'list-videos', '', current_page + 1, last_page, search_string, categories, meta)
+	xbmcplugin.endOfDirectory(pluginhandle)
+	return True
+
+def get_stream_url(video_id):
+	debug("(get_stream_url) -------------------------------------------------- START = get_stream_url --------------------------------------------------")
+	tokenURL = ""
+	# https://tv.dfb.de/server/streamAccess.php?videoId=22862&target=17&partner=2180&label=web_dfbtv&area=testarea&format=iphone
+	URL_1 = 'https://tv.dfb.de/server/hd_video.php?play=' + video_id
+	debug("(get_stream_url) ### xml-URL_1 : "+URL_1+" ###")
+	try:
+		DATA_1 = getUrl(URL_1)
+		debug("(get_stream_url) ### xml-DATA_1 : "+DATA_1+" ###")
+		URL_2 = clean_text(re.findall('<url>(.+?)</url>', DATA_1, re.S)[0]).split('?')[0] + '?format=iphone'
+		DATA_2 = getUrl('http:' + URL_2.strip())
+		videoURL = re.findall('url="([^"]+?)"', DATA_2, re.S)[0].replace('&amp;', '&')
+		debug("(get_stream_url) ### Video-URL : "+videoURL+" ###")
+		tokenURL = re.findall('auth="([^"]+?)"', DATA_2, re.S)[0].replace('&amp;', '&')
+		debug("(get_stream_url) ### Token-URL : "+tokenURL+" ###")
+	except: return False
+	startURL = videoURL[:videoURL.find('_,')+2]
+	if tokenURL != "": endURL = '?hdnea='+tokenURL
+	else: endURL = ""
+	debug("(get_stream_url) ### VIDEO-URL : "+startURL+'XXX,.mp4.csmil/index_0_av.m3u8'+endURL+" ###")
+	if not ',1200,' in videoURL:
+		if not ',800,' in videoURL:
+			if not ',500,' in videoURL:
+				if not ',300,' in videoURL: return False
+				return startURL+'300,.mp4.csmil/index_0_av.m3u8'+endURL
+			return startURL+'500,.mp4.csmil/index_0_av.m3u8'+endURL
+		return startURL+'800,.mp4.csmil/index_0_av.m3u8'+endURL
+	if HD: return startURL+'1200,.mp4.csmil/index_0_av.m3u8'+endURL
+	return startURL+'800,.mp4.csmil/index_0_av.m3u8'+endURL
+
+def list_all_livestreams():
+	debug("(list_all_livestreams) -------------------------------------------------- START = list_all_livestreams --------------------------------------------------")
+	try:
+		html = getUrl('https://tv.dfb.de/static/livestreams/')
+		videos = re.findall('<div class="video-teaser.*?<img src=".*?/images/(.+?)_.*?subline">(.+?)</.*?-headline">(.+?)</', html, re.S)
+	except: return False
+	item_added = False
+	for video in videos:
+		try:
+			video_id = video[0].strip()
 			subline = clean_text(video[1])
 			title = clean_text(video[2])
 			if HD: thumb = 'https://search.dfb.de/proxy/large_hires?url=https://tv.dfb.de/images/' + video_id + '_1360x760.jpg'
@@ -162,6 +224,44 @@ def list_all_livestreams(live_url = 'https://tv.dfb.de/static/livestreams/'):
 	xbmcplugin.endOfDirectory(pluginhandle)
 	return True
 
+def get_live_url(video_id):
+	debug("(get_live_url) -------------------------------------------------- START = get_live_url --------------------------------------------------")
+	videoURL = ""
+	tokenURL = ""
+	# https://tv.dfb.de/server/streamAccess.php?videoId=22862&target=17&partner=2180&label=web_dfbtv&area=testarea&format=iphone
+	URL_1 = 'https://tv.dfb.de/server/hd_video.php?play=' + video_id
+	debug("(get_live_url) ### xml-URL_1 : "+URL_1+" ###")
+	try:
+		DATA_1 = getUrl(URL_1)
+		debug("(get_live_url) ### xml-DATA_1 : "+DATA_1+" ###")
+		if '<live>true</live>' in DATA_1:
+			if not ('<islive>true</islive>' in DATA_1):
+				debug("(get_live_url) ### LIVE-URL = NO--islive--Found ###")
+				return False
+		URL_2 = clean_text(re.findall('<url>(.+?)</url>', DATA_1, re.S)[0]).split('?')[0] + '?format=iphone'
+		DATA_2 = getUrl('http:' + URL_2.strip())
+		videoURL = re.findall('url="([^"]+?)"', DATA_2, re.S)[0].replace('&amp;', '&')
+		debug("(get_live_url) ### Video-URL : "+videoURL+" ###")
+		tokenURL = re.findall('auth="([^"]+?)"', DATA_2, re.S)[0].replace('&amp;', '&')
+		debug("(get_live_url) ### Token-URL : "+tokenURL+" ###")
+	except: return False
+	if videoURL.endswith('.m3u8') and tokenURL != "":
+		debug("(get_live_url) ### LIVE-URL : "+videoURL+'?hdnea='+tokenURL+" ###")
+		return videoURL+'?hdnea='+tokenURL
+	else:
+		debug("(get_live_url) ### LIVE-URL = NO--Token--Found ###")
+		return False
+
+def play_video(title, stream, thumb):
+	debug("(play_video) -------------------------------------------------- START = play_video --------------------------------------------------")
+	log("(play_video) streamURL : "+stream)
+	listitem = xbmcgui.ListItem(path=stream)
+	listitem.setInfo(type="Video", infoLabels={'Title': title})
+	listitem.setArt({'thumb': thumb, 'fanart': thumb})
+	listitem.setMimeType('application/vnd.apple.mpegurl')
+	listitem.setContentLookup(False)
+	xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
+
 def get_search_string():
 	debug("(get_search_string) -------------------------------------------------- START = get_search_string --------------------------------------------------")
 	keyboard = xbmc.Keyboard('', translation(30609))
@@ -170,46 +270,26 @@ def get_search_string():
 		return keyboard.getText().replace(" ", "+")
 	return False
 
-def list_all_videos(search_string = '', categories = '', current_page = 1, last_page = 0, meta = ''):
-	debug("(list_all_videos) -------------------------------------------------- START = list_all_videos --------------------------------------------------")
-	json_url = 'http://search.dfb.de/search/videos.json?'
-	if search_string: json_url += 'q=' + search_string + '&'
-	if categories: json_url += 'categories=' + categories + '&'
-	if meta: json_url += 'meta_attribute_ids' + meta + '&'
-	json_url += 'page=' + str(current_page)
-	debug("(list_all_videos) ### Json_url : "+json_url+" ###")
-	try:
-		json = getUrl(json_url)
-		if HD: regex_video_info = '"guid":.*?"video-(.+?)","title":"(.+?)",.*?"medium_hires":"(.+?)",.*?"pub_date":"(.+?)"'
-		else: regex_video_info = '"guid":.*?"video-(.+?)","title":"(.+?)",.*?"image_url":"(.+?)",.*?"pub_date":"(.+?)"'
-		videos = re.findall(regex_video_info, json, re.DOTALL)
-	except: return False
-	item_added = False
-	for video in videos:
-		try:
-			video_id = video[0]
-			title = clean_text(video[1])
-			if HD: thumb = video[2].replace('medium_hires', 'large_hires')
-			else: thumb = video[2].replace('medium?', 'medium_hires?')
-			date = video[3].strip()
-			add_video(date+' - '+title, video_id, thumb, 'play-video')
-			debug("(list_all_videos) ### Title : "+str(date)+' - '+title+" ###")
-			debug("(list_all_videos) ### Video_id : "+str(video_id)+" ###")
-			debug("(list_all_videos) ### Thumb : "+thumb+" ###")
-			item_added = True
-		except:
-			failing("(list_all_videos) ERROR - ERROR - ERROR ### {0} ###".format(str(video)))
-	if not item_added: return warning(translation(30523))
-	if not last_page: 
-		regex_total = '"total":(.+?),'
-		try: last_page = get_last_page(int(re.findall(regex_total, json)[0]))
-		except: last_page = current_page
-		debug("(list_all_videos) ### Last_page : "+str(last_page)+" ###")
-	if current_page < last_page:
-		debug("(list_all_videos) ### Current_page : "+str(current_page)+" ### Last_page : "+str(last_page)+" ###")
-		add_category((translation(30610).format(current_page+1)), 'list-videos', '', current_page + 1, last_page, search_string, categories, meta)
-	xbmcplugin.endOfDirectory(pluginhandle)
-	return True
+def get_last_page(total_results):
+	if not total_results: return 0
+	if total_results % 10 == 0: return total_results // 10
+	return total_results // 10 + 1
+
+def clean_text(text):
+	return text.replace('\n', '').replace('\t', '').replace('&amp;', '&').replace('   ', '').replace('  ', ' ').replace('\\"', '"').replace('\\u0026', '&').replace('\\t', '').replace(': :', ':').strip()
+
+def warning(text, title = 'DFB TV :', showTime = 8000):
+	xbmcgui.Dialog().notification(title, text, icon, int(showTime))
+
+def parameters_string_to_dict(parameters):
+	paramDict = {}
+	if parameters:
+		paramPairs = parameters[1:].split("&")
+		for paramsPair in paramPairs:
+			paramSplits = paramsPair.split('=')
+			if (len(paramSplits)) == 2:
+				paramDict[paramSplits[0]] = paramSplits[1]
+	return paramDict
 
 def add_video(title, video_id, thumb, mode, desc = ''):
 	link = sys.argv[0] + "?id=" + video_id + "&title=" + quote_plus(title) + "&thumb=" + quote_plus(thumb) + "&mode=" + str(mode)
@@ -235,75 +315,6 @@ def add_category(title, mode, thumb = '', current_page = 1, last_page = 0, searc
 		liz.setArt({'fanart': defaultFanart})
 	return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=link, listitem=liz, isFolder=True)
 
-def play_video(title, stream, thumb):
-	debug("(play_video) -------------------------------------------------- START = play_video --------------------------------------------------")
-	log("(play_video) streamURL : "+stream)
-	listitem = xbmcgui.ListItem(path=stream)
-	listitem.setInfo(type="Video", infoLabels={'Title': title})
-	listitem.setArt({'thumb': thumb, 'fanart': thumb})
-	listitem.setMimeType('application/vnd.apple.mpegurl')
-	listitem.setContentLookup(False)
-	xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
-
-def get_stream_url(video_id):
-	debug("(get_stream_url) -------------------------------------------------- START = get_stream_url --------------------------------------------------")
-	token_url = ""
-	# https://tv.dfb.de/server/streamAccess.php?videoId=22862&target=17&partner=2180&label=web_dfbtv&area=testarea&format=iphone
-	xml_url = 'https://tv.dfb.de/server/hd_video.php?play=' + video_id
-	debug("(get_stream_url) ### Xml_url : "+xml_url+" ###")
-	try:
-		xml_data = getUrl(xml_url)
-		debug("(get_stream_url) ### Xml_data : "+xml_data+" ###")
-		if '<live>true</live>' in xml_data:
-			if not ('<islive>true</islive>' in xml_data):
-				debug("(get_stream_url) ### LIVE-URL = NO--islive--Found ###")
-				return False
-		regex_url = '<url>(.+?)</url>'
-		xml_url_video = clean_text(re.findall(regex_url, xml_data, re.DOTALL)[0]).split('?')[0] + '?format=iphone'
-		xml_url_video = 'http:' + xml_url_video.strip()
-		xml_data_video = getUrl(xml_url_video)
-		regex_url_video = 'url="([^"]+?)"'
-		video_url = re.findall(regex_url_video, xml_data_video, re.DOTALL)[0].replace('&amp;', '&')
-		debug("(get_stream_url) ### Video_url : "+video_url+" ###")
-		try:
-			regex_auth_video = 'auth="([^"]+?)"'
-			token_url = re.findall(regex_auth_video, xml_data_video, re.DOTALL)[0].replace('&amp;', '&')
-			debug("(get_stream_url) ### Token_url : "+token_url+" ###")
-		except: pass
-	except: return False
-	if '/dfb_live' in video_url and video_url.endswith('.m3u8') and token_url != "":
-		debug("(get_stream_url) ### LIVE-URL : "+video_url+'?hdnea='+token_url+" ###")
-		return video_url+'?hdnea='+token_url
-	elif '/dfb_live' in video_url and video_url.endswith('.m3u8') and token_url == "":
-		debug("(get_stream_url) ### LIVE-URL = NO--Token--Found ###")
-		return False
-	startURL = video_url[:video_url.find('_,')+2]
-	if token_url != "": endURL = '?hdnea='+token_url
-	else: endURL = ""
-	debug("(get_stream_url) ### VIDEO-URL : "+startURL+'XXX,.mp4.csmil/index_0_av.m3u8'+endURL+" ###")
-	if not ',1200,' in video_url:
-		if not ',800,' in video_url:
-			if not ',500,' in video_url:
-				if not ',300,' in video_url: return False
-				return startURL+'300,.mp4.csmil/index_0_av.m3u8'+endURL
-			return startURL+'500,.mp4.csmil/index_0_av.m3u8'+endURL
-		return startURL+'800,.mp4.csmil/index_0_av.m3u8'+endURL
-	if HD: return startURL+'1200,.mp4.csmil/index_0_av.m3u8'+endURL
-	return startURL+'800,.mp4.csmil/index_0_av.m3u8'+endURL
-
-def parameters_string_to_dict(parameters):
-	paramDict = {}
-	if parameters:
-		paramPairs = parameters[1:].split("&")
-		for paramsPair in paramPairs:
-			paramSplits = paramsPair.split('=')
-			if (len(paramSplits)) == 2:
-				paramDict[paramSplits[0]] = paramSplits[1]
-	return paramDict
-
-def warning(text, title = 'DFB TV :', showTime = 8000):
-	xbmcgui.Dialog().notification(title, text, icon, int(showTime))
-
 params = parameters_string_to_dict(sys.argv[2])
 mode = unquote_plus(params.get('mode', ''))
 id = unquote_plus(params.get('id', ''))
@@ -316,19 +327,12 @@ cat = unquote_plus(params.get('categories', ''))
 meta_ids = unquote_plus(params.get('meta', ''))
 referer = unquote_plus(params.get('referer', ''))
 
-if mode == 'list-livestreams':
-	if not list_all_livestreams(): warning(translation(30524))
-elif mode == 'play-livestream':
-	stream_url = get_stream_url(id)
-	if stream_url: play_video(title, stream_url, thumb)
-	else: warning(translation(30525))
+if mode == 'list-videos':
+	if not list_all_videos(search_st, cat, int(c_page), int(l_page), meta_ids): warning(translation(30524))
 elif mode == 'play-video':
 	stream_url = get_stream_url(id)
 	if stream_url: play_video(title, stream_url, thumb)
-	else: warning(translation(30526))
-elif mode == 'search':
-	search_s = get_search_string()
-	if search_s: list_all_videos(search_string = search_s)
+	else: warning(translation(30525))
 elif mode == 'category-men':
 	debug("(category-men) -------------------------------------------------- START = category-men --------------------------------------------------")
 	add_category(title = translation(30611), mode = 'list-videos', categories = 'Männer')
@@ -347,7 +351,14 @@ elif mode == 'category-women':
 	add_category(title = translation(30620), mode = 'list-videos', categories = 'DFB-Pokal+der+Frauen')
 	add_category(title = translation(30621), mode = 'list-videos', search_string = 'juniorinnen')
 	xbmcplugin.endOfDirectory(pluginhandle)
-elif mode == 'list-videos':
-	if not list_all_videos(search_st, cat, int(c_page), int(l_page), meta_ids): warning(translation(30527))
+elif mode == 'list-livestreams':
+	if not list_all_livestreams(): warning(translation(30526))
+elif mode == 'play-livestream':
+	live_url = get_live_url(id)
+	if live_url: play_video(title, live_url, thumb)
+	else: warning(translation(30527))
+elif mode == 'search':
+	search_s = get_search_string()
+	if search_s: list_all_videos(search_string = search_s)
 else:
 	index()
